460cc6515d693dca6398589a63fe13b8148b39dd
kyuhyoung <kyuhyoung@gmail.com>
2017-08-23 08:31:55 +0900

    Change to optimization form

--- main.py
+++ main.py
@@ -5,14 +5,20 @@ import os, csv
 import matplotlib.pyplot as plt
 import numpy as np
 
-def find_main_valley(li_val):
+def compute_loss(li_val, bin_int, pos, tgt):
+    valley = find_main_valley(li_val, bin_int, pos)
+    loss = abs(valley - tgt)
+    return loss
+
+
+def find_main_valley(li_val, bin_int, pos):
     #   히스토 그램을 그린다.
     '''
     plt.hist(li_val, bins='auto')
     plt.title("Histogram with 'auto' bins")
     plt.show()
     '''
-    bin_int = 20
+    #bin_int = 20
     fig = plt.figure()
     ax = fig.add_subplot(111)
     n, b, patches = ax.hist(li_val, bins='auto')
@@ -35,8 +41,9 @@ def find_main_valley(li_val):
     for i in xrange(bin_from, bin_to):
         t1 = b[i]
         print(t1)
-    bin_center = (bin_from + bin_to) / 2
+    bin_center = bin_from + int(round(bin_int * pos))
     bin_thres = b[bin_center]
+    plt.close(fig)
     return bin_thres
 
 
@@ -52,9 +59,10 @@ def get_csv_reader(filename, delimiter):
     #return list(reader)
     return reader
 
+def get_target_value():
+    return 0
 
-
-def process(fn_csv, li_coi):
+def process(fn_csv, li_coi, bin_int, pos):
     #   csv 파일을 읽는다
     li_col_data = get_csv_reader(fn_csv, ',')
     di_col_li_val = {}
@@ -73,10 +81,15 @@ def process(fn_csv, li_coi):
                     di_col_li_val[header] = [val]
             #   건너 뛴다
         #   데이터에서 계곡을 찾는다.
+    loss_total = 0
     for col, li_val in di_col_li_val.items():
-        valley = find_main_valley(li_val)
-        di_col_valley[col] = valley
-    return di_col_valley
+        tgt = get_target_value()
+        loss = compute_loss(li_val, bin_int, pos, tgt)
+        loss_total += loss
+        #valley = find_main_valley(li_val)
+        #di_col_valley[col] = valley
+    #return di_col_valley
+    return loss_total
 
 def get_list_of_files_with_string_under_subfolders(folder_name, li_str):
     '''Makes list of file names which includes any of given strings under any sub-folders of the given folder
@@ -128,14 +141,27 @@ def parse_args():
 
 def main():
     #   인자를 파싱한다.
-    li_pc, li_coi, dir_csv = parse_args()
+    li_pc, li_coi, bin_int_range, n_div, dir_csv = parse_args()
     li_fn_csv = \
         get_list_of_files_with_string_under_subfolders(
             dir_csv, li_pc)
-    #   각 폴더에 대해 파일에 대해
-    for fn_csv in li_fn_csv:
-        process(fn_csv, li_coi)
-        #   프로세스를 한다.
+    bin_int_min, bin_int_max = bin_int_range
+    bin_int_best = -1
+    pos_best = -1
+    min_loss = 10000000000000000000000000000000.0
+    for bin_int in xrange(bin_int_min, bin_int_max):
+        for i in xrange(n_div + 1):
+            pos = float(i) / float(n_div)
+            #   각 폴더에 대해 파일에 대해
+            for fn_csv in li_fn_csv:
+                loss_total = process(fn_csv, li_coi, bin_int, pos)
+                if loss_total < min_loss:
+                    min_loss = loss_total
+                    bin_int_best = bin_int
+                    pos_best = pos
+                #   프로세스를 한다.
+    print(bin_int_best)
+    print(pos_best)
     return
 
 # usage
